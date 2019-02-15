828d12ea4ded70b8aa6749d504683daed57c64cd
Eldson <eldsonlm@gmail.com>
2019-02-14 21:19:52 -0300

    projeto das telas e estruturacao

---
+++ LocacaoEquipamento/src/Principal/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author Elisson
+ */
+public class Main {
+    public static void main (String[] args){
+        new Login().setVisible(true);
+    }
+}
