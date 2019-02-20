cd0f7633abd8880fcf8f3e55b173a062c43264dc
raiaane <costaraiane25@gmail.com>
2019-02-20 14:12:31 -0300

    Modelo Marca e Tipo

---
+++ src/Modelo/Marca.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Raiane
+ */
+public class Marca {
+    private int cod;
+    private String nome;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
